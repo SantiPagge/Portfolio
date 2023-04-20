80e965d3773b7f2a40c5d6fb11de756631b51eac
SantiPagge <santy.pagge@gmail.com>
2023-04-20 20:01:06 -0300

    agregue imagen de mi hermosa cara

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <div>Este es mi portfolio con Next.js</div>
+    <div>
+      <div className="bg-blue-600 flex justify-center font-bold text-3xl py-4">Este es mi portfolio con Next.js</div>
+      <div>
+        <Image src='/Santu.jpg' alt='santu' className='rounded-full w-52' width={500} height={500}/>
+      </div>
+    </div>
   )
 }
