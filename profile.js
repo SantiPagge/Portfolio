75a4fc191051fd0643f080731afb3f5c89cc52ca
Santiago Pagge <santy.pagge@gmail.com>
2023-04-21 18:35:09 -0300

    algunos estilos

---
+++ profile.js
@@ -0,0 +1,20 @@
+export const projects = [
+    {
+        title: 'Fusionajob',
+        description: 'Fusionajob es un sitio web para búsqueda de empleo y contratación en el campo IT.',
+        image: '/fusionajob.jpg',
+        url: 'https://fusionajob.vercel.app/'
+    },
+    {
+        title: 'Henry Foods',
+        description: 'Página web de recetas.',
+        image: '/henryfoods.jpg',
+        url: 'https://santipagge-pifoodshenry.vercel.app/'
+    },
+    // {
+    //     title: 'Rick and Morty',
+    //     description: 'Proyecto en progreso, comenzado durante el bootcamp Soy Henry para afinzar conceptos, creada con React, CSS module, Express.js, Sequealize, PostgreSQL, GitHub',
+    //     image: 'https://r1.abcimg.es/resizer/resizer.php?imagen=https%3A%2F%2Fs3.abcstatics.com%2Fmedia%2Fseries%2F000%2F003%2F130%2Frick-y-morty-1.jpg&nuevoancho=320&medio=abc',
+    //     url: 'https://github.com/L34ndro0/Rick_And_Morty'
+    // },
+]
