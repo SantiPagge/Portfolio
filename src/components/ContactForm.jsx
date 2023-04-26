8793b5bf63d6c9eaa927db55e8ce2823655d728d
Santiago Pagge <santy.pagge@gmail.com>
2023-04-26 20:07:19 -0300

    Actualizando form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,10 +1,26 @@
 export const ContactForm = () => {
+
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: ''
+  })
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setForm({
+      name: '',
+      enail: '',
+      message: ''
+    })
+  }
+
     return (
-    <div className=''>
-          <form className=''>
-            <p>Nombre:</p><input className='' type='text'/>
-            <p>Email:</p><input className='' type='email'/>
-            <p>Mensaje:</p><input className='' type='textarea'/>
+    <div className='text-black'>
+          <form className='' onSubmit={handleSubmit}>
+            <p>Nombre:</p><input type='text' id='name' value={form.name}/>
+            <p>Email:</p><input type='email' id='email' value={form.email}/>
+            <p>Mensaje:</p><textarea rows='4' cols='50'></textarea>
             <button type='submit'>Enviar</button>
           </form>
         </div>
