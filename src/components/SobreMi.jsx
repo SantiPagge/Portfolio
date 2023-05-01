7827d5d42f5a30e35bf580076ff72cbe0b27c360
SantiPagge <santy.pagge@gmail.com>
2023-05-01 20:38:34 -0300

    agregue cv

--- src/components/SobreMi.jsx
+++ src/components/SobreMi.jsx
@@ -19,6 +19,9 @@ export const SobreMi = () => {
             <a href='https://www.linkedin.com/in/santiagopagge/' target="_blank" rel="noopener noreferrer"><Image src='/linkedin.png' alt='linkedin' width={30} height={30} className='mx-4 hover:scale-125 transform transition-transform duration-500'/></a>
             <a href='https://wa.me/542364347911' target="_blank" rel="noopener noreferrer"><Image src='/whatsapp.png' alt='linkedin' width={30} height={30} className='hover:scale-125 transform transition-transform duration-500'/></a>
             </div>
+            <div className='flex justify-center pt-4'>
+                <a href='https://drive.google.com/file/d/1Z8fFfUQWezDEMm3NUgoTcbrT4bbS5Zy5/view?usp=share_link' target="_blank" rel="noopener noreferrer"><button className='bg-blue-800 w-auto rounded-md p-2 font-bold'>Ver mi CV</button></a>
+            </div>
             </div>
         </div>
     )
