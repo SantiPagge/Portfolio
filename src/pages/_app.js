aed336df3e3a36e82144afc5d163a3bd5096903f
SantiPagge <santy.pagge@gmail.com>
2023-05-18 20:40:38 -0300

    nombre en pestaña de navegador

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,14 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
 import { LanguageProvider } from '../../LanguageContext';
 
 export default function App({ Component, pageProps }) {
   return (
   <LanguageProvider>
+    <Head>
+      {/* <link rel='icon' href='../../public/burger.png'/> */}
+      <title>Santiago Pagge</title>
+    </Head>
     <Component {...pageProps} />
   </LanguageProvider>
   );
