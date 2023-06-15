4a42996f0b98bb79c62204f06fd06e24f7295172
Santiago Pagge <santy.pagge@gmail.com>
2023-06-15 16:48:54 -0300

    Solucionado tema fuentes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,4 @@
 import '@/styles/globals.css';
-import { GoogleFonts } from 'next-google-fonts';
 import Head from 'next/head';
 import { LanguageProvider } from '../../LanguageContext';
 
@@ -10,7 +9,6 @@ export default function App({ Component, pageProps }) {
       <link rel='icon' href='/burger.png'/>
       <title>Santiago Pagge</title>
     </Head>
-    <GoogleFonts href='https://fonts.googleapis.com/css2?family=Kanit&display=swap' />
     <Component {...pageProps} />
   </LanguageProvider>
   );
