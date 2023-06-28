f8dfd23f3ac7a1315e06c1233b225b0aafe14040
Santiago Pagge <santy.pagge@gmail.com>
2023-06-28 17:08:05 -0300

    fondo cards

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -15,7 +15,7 @@ export const Proyectos = () => {
           rel="noopener noreferrer"
           className='inline-block align-middle hover:scale-105 transform transition-transform duration-500 mb-5'
         >
-          <div className='m-4 w-80 h-full border-4 border-black rounded-2xl overflow-hidden shadow-lg' key={index}>
+          <div className='m-4 w-80 h-full border-4 border-black bg-blue-700 rounded-2xl overflow-hidden shadow-lg' key={index}>
             <div>
               <div>
                 <img src={image} alt={title} className='rounded-t-2xl border-b-4 border-black'/>
