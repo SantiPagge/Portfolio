13780cf3d766a7d7b6ec253397eade3ec6ea8695
SantiPagge <santy.pagge@gmail.com>
2023-07-06 16:24:44 -0300

    Responsive ready

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,14 +21,14 @@ export default function Home() {
         </div>
         <div className="grid sm:grid-cols-2 bg-blue-900 h-96">
           <div className='flex flex-col text-center justify-top'> 
-            <h1 className='font-bold lg:text-6xl md:4xl sm:2xl text-white w-auto sm:mt-10 sm:ml-32'>Santiago Pagge</h1>
+            <h1 className='font-bold lg:text-6xl md:4xl text-4xl text-white w-auto sm:mt-10 sm:ml-32'>Santiago Pagge</h1>
             <p className='font-bold lg:text-2xl md:4xl sm:2xl text-white w-auto sm:mt-4 sm:ml-32'>Full Stack Web Developer</p>
           <div className='hidden sm:inline'>
             <Tecnologias/>
           </div>
           </div>
           <div className='flex justify-center items-center'>
-            <Image src='/Santu.jpg' alt='santu' className='rounded-full lg:w-80 md:w-60 sm:w-60 w-32 border-4 border-slate-950 shadow-lg shadow-slate-900 mr-32' width={500} height={500}/>
+            <Image src='/Santu.jpg' alt='santu' className='rounded-full sm:mr-32 lg:w-80 md:w-60 sm:w-60 w-32 border-4 border-slate-950 shadow-lg shadow-slate-900' width={500} height={500}/>
           </div>
           <div className='sm:hidden inline'>
             <Tecnologias/>
